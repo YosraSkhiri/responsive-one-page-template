b40690d0e3b351b239c45fd2a2831b57e75ec69e
Yosra <yos.skhiri@gmail.com>
2019-09-03 15:30:17 +0100

    added the mobile menu

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 hamburger = document.querySelector('.menu');
-navbar = document.querySelector('.links > ul');
+navbar = document.querySelector('.links');
+navbar_atags = document.querySelectorAll('.links > ul a');
+sign_up = document.querySelector('.sign-up-btn');
 
 hamburger.addEventListener("click", function(){
-    //navbar.classList.add('mob-nav');
-
+    navbar.classList.toggle('links');
+    sign_up.classList.toggle('sign-up-btn');
+    navbar_atags.forEach(element => {
+        element.classList.toggle('link');
+    });
+    navbar.classList.toggle('mob-nav');
 });
